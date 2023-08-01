fd763f83bdc271d52896f5938cd4f7e56f403827
kaiquedlf <kaiquedlf@gmail.com>
2023-08-01 19:48:00 -0300

    finish

--- random-quote/src/assets/index.jsx
+++ random-quote/src/assets/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import './index.css'
 import colors from './colors';
 
@@ -7,40 +6,69 @@ import colors from './colors';
 export default function Index(){
     const [quotes, setQuotes] = useState([]);
     const [colorsDatas, setColorsDatas] = useState([]);
+    const [number, setNumber] = useState(0);
     
+    /* Make calls to api to get a random quote */
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://api.quotable.io/quotes/random');
-        setQuotes(response.data[0]);
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-      }
-    };
+    useEffect(() => {
 
-    fetchData();
-    setColorsDatas(colors)
-  }, []);
-  
+      fetch('https://api.quotable.io/quotes/random')
+          .then(res => res.json())
+          .then(data => setQuotes(data[0]))
+      
+          setColorsDatas(colors)
+
+    }, [number]);
+
+
+    /* Add + 1 to state "number" to re-render the component and make the api call again */
+    
+    function handleChange(){
+      setNumber(nb => nb + 1)
+    }
+
+    /* Get a random color from colors.jsx */
 
   const color = Object.values(colorsDatas);
 
   const randomColor = color[Math.floor(Math.random() * color.length)]
 
-    const style = {
-        color: randomColor
-    }
+    
   
     return(
-        <div className='page-container' style={{backgroundColor: randomColor}}>
-            <div className="quote-container">
-                <h1 className='content-container' style={style}>{quotes.content}</h1>
-                <small className='author-container' style={style}>- {quotes.author}</small>
+        <div 
+        className='page-container' 
+        style={{backgroundColor: randomColor, 
+                transition: "background-color 0.5s ease-in-out"}}>
+            <div 
+            className="quote-container">
+                <h1 
+                className='content-container' 
+                style={{color: randomColor, 
+                        transition: "0.5s ease-in-out"}}>
+                  {quotes.content}
+                  </h1>
+                <small 
+                className='author-container' 
+                style={{color: randomColor, 
+                        transition: " 0.5s ease-in-out"}}>
+                  - {quotes.author}
+                  </small>
 
-                <div className="btns">
-                    <div className="btn-links"></div>
-                    <button className='btn-newquote' style={{backgroundColor: randomColor, color: "white"}}>New Quote</button>
+                <div 
+                className="btns">
+                    <div 
+                    className="btn-links">
+
+                    </div>
+                    <button 
+                    onClick={handleChange}
+                    className='btn-newquote' 
+                    style={{backgroundColor: randomColor, 
+                            color: "white",
+                            transition: "background-color 0.5s ease-in-out"}}>
+                      New Quote
+                      </button>
                 </div>
             </div>
         </div>
--- random-quote/src/main.jsx
+++ random-quote/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import Index from './assets'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+
+ReactDOM.createRoot(document.getElementById
+  ('root')).render(
     <Index />
-  </React.StrictMode>,
 )
